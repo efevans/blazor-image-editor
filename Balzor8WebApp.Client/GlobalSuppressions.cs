b0190bf6a62a952f6a78c83d6df3672557908d99
efevans <efevans1@gmail.com>
2024-04-30 11:32:23 -0700

    Add supression file for platform compatibility

---
+++ Balzor8WebApp.Client/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:Balzor8WebApp.Client.ImagePostProcessing.FloydSteinbergDitherer.ParseOptions(System.Collections.Generic.Dictionary{System.String,Balzor8WebApp.Client.Pages.ArtCanvas.CanvasEffectOption})~Balzor8WebApp.Client.ImagePostProcessing.FloydSteinbergDitherer.Options")]
